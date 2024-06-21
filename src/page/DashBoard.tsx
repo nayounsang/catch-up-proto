50d36167fe4b72f81f9a2c10288efaf656ec8ec3
nayounsang <nayounsang722@gmail.com>
2024-06-21 15:14:52 +0900

    feat: 홈페이지 ui 제작

---
+++ src/page/DashBoard.tsx
@@ -0,0 +1,19 @@
+import { styled } from "@mui/material";
+
+export default function DashBoard() {
+  return (
+    <>
+      <Container>
+
+      </Container>
+    </>
+  );
+}
+
+const Container = styled("section")(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "100vh",
+}));
