3bef936eeeb413eeed72fecde2cdfe103dfe29d8
nayounsang <nayounsang722@gmail.com>
2024-06-26 14:29:20 +0900

    fix: 사이드바 메뉴 누르면 스크롤 초기화되는거 수정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,10 +3,11 @@ import Home from "../page/Home";
 import Login from "../page/Login";
 import DashBoard from "../page/DashBoard";
 import Session from "../page/Session";
-import DashBoardLayout from "../component/layout/DashBoardLayout";
 import DashboardSessions from "../page/DashBoard/page/Sessions";
 import DashBoardDocuments from "../page/DashBoard/page/Documents";
 import DashBoardChildrenLayout from "@/component/layout/\bDashBoardLayout/DashBoardChildrenLayout";
+import Drive from "@/page/DashBoard/page/Drive";
+import DashBoardLayout from "@/component/layout/\bDashBoardLayout";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -22,7 +23,7 @@ const router = createBrowserRouter([
         children: [
           { path: "/dashboard/sessions", element: <DashboardSessions /> },
           { path: "/dashboard/documents", element: <DashBoardDocuments /> },
-          { path: "/dashboard/drive", element: <Home /> },
+          { path: "/dashboard/drive", element: <Drive /> },
         ],
       },
     ],
